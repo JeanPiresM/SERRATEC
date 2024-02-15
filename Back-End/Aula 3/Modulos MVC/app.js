94ddb576b963d902655e7ce0b4347d18cb400e0d
JeanPiresM <jeanpires223@gmail.com>
2024-02-15 18:27:57 -0300

    Aula 3 de Back-End

--- Back-End/Aula 3/Modulos MVC/app.js
+++ Back-End/Aula 3/Modulos MVC/app.js
@@ -7,7 +7,7 @@ const post = require("./models/post");
 //caso ela não exista se tiver postMessage.sync({force:true})
 //ele irá deletar a tabela e criar outra vez
 const postagem = require("./models/post");
-const port = 2023;
+const port = 3007;
 //configurar passagem do método post pelo proprio express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -28,24 +28,66 @@ app.engine(
     },
   })
 );
-//criação da rota formulario que está sendo renderizada com 
+//criação da rota formulario que está sendo renderizada com
 //o arquivo formulario.handlebars
-app.get("/formulario", function(req,res){
-  res.render('layouts/formulario')
-}) 
-app.get("/editar", function(req,res){
-  res.render('layouts/editar')
-})
+app.get("/formulario", function (req, res) {
+  res.render("layouts/formulario");
+});
+app.get("/editar", function (req, res) {
+  res.render("layouts/editar");
+});
 
-app.post("/atualizar", function(req,res){
+app.post("/atualizar", function (req, res) {
   // id = req.body.id
-  const idPost = req.body.id
+  const idPost = req.body.id;
   /*postagem é a variavel que alem de realizar a conexão com
  o banco de dados criou a entidade que ira atualizar dados na 
  tabela */
 
- postagem.update({
-  titulo: req.body.titulo,
-  conteudo: req.body.conteudo
- })
-})
+  postagem
+    .update(
+      {
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo,
+      },
+      { where: { id: idPost } }
+    )
+    .then(function () {
+      res.send("Atualizado com sucesso");
+    })
+    .catch(function (erro) {
+      res.send("Arquivo não atualizado" + erro);
+    });
+});
+
+app.get("/listar", function (res, req) {
+  postagem.allowMethods;
+  postagem.findAll().then(function (posts) {
+    res.render("layouts/listar", { p: post });
+  });
+});
+
+app.get("/home", function (req, res) {
+  res.render("layouts/home");
+});
+
+app.post("/salvar", function (req, res) {
+  postagem
+    .create({
+      titulo: req.body.titulo,
+      conteudo: req.body.conteudo,
+    })
+    .then(function () {
+      res.send("Criado com sucesso");
+    })
+    .catch(function (erro) {
+      res.send("Houve um erro" + erro);
+    });
+});
+
+app.set("view engine", "handlebars");
+
+app.listen(port, function () {
+  console.log(`Servidor rodando na porta localhost:${port}`);
+});
+
--- Back-End/Aula 3/Modulos MVC/models/db.js
+++ Back-End/Aula 3/Modulos MVC/models/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(
   'blog','root','', {
     host: "localhost",
-    dialect: "MariaDB"
+    dialect: "mariadb"
   }
 )
 
