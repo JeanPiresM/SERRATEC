869d17e4a1f16fdbd2854a7fe3e4ee8c2df92fb2
JeanPiresM <jeanpires223@gmail.com>
2024-02-24 12:52:08 -0300

    Monitorias 5, 6 e 7

--- Back-End/Aula 4/API Mariadb/src/index.js
+++ Back-End/Aula 4/API Mariadb/src/index.js
@@ -1,12 +1,23 @@
-// const Express = require('express')
-// const app = Express()
-// const port = 4100
+// Importa o módulo "express", que é um framework para construir aplicativos web em Node.js
+const express = require("express");
 
+// Importa o módulo "cors" para lidar com políticas de mesma origem (Cross-Origin Resource Sharing)
+const cors = require('cors');
 
-// app.get("/", function(req,res){
-//   res.end('Minha pagina principal')
-// })
+// Cria uma instância do servidor Express
+const api = express();
 
-// app.listen(port,function(){
-//   console.log(`Servidor rodando em localhost://${port}`)
-// })
+// Adiciona middleware para processar dados no formato JSON
+api.use(express.json());
+
+// Importa as rotas definidas no arquivo "router.js"
+const routes = require("./router");
+
+// Adiciona o middleware CORS para permitir solicitações de origens diferentes
+api.use(cors());
+
+// Adiciona as rotas ao servidor Express
+api.use(routes);
+
+// Inicia o servidor na porta 4100
+api.listen(4100);
---
+++ monitoria/monitoria 7 24-02 UC5/pagina-noticias/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContentComponent } from './components/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    MenuComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
